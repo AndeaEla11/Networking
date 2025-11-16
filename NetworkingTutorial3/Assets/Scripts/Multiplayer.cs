29bd980e25aa2c4175877fd6d3405101f1354611
Andreea Straulescu <andreaela99@gmail.com>
2025-11-16 00:39:50 +0000

    Networking V004

---
+++ NetworkingTutorial3/Assets/Scripts/Multiplayer.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class Multiplayer : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public new Rigidbody rigidbody;
+
+    [HideInInspector]
+    public int health = 100;
+    public Slider healthBar;
+
+    PhotonView photonView;
+
+    public float fireRate = 0.75f;
+    public GameObject bulletPrefab;
+    public Transform bulletPosition;
+    float nextFire;
+    public GameObject bulletFiringEffect;
+    public AudioClip playerShootingAudio;
+
+    
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        photonView = GetComponent<PhotonView>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if(!photonView.IsMine)
+            return;
+
+        Move();
+        if (Input.GetKey(KeyCode.Space))
+            Fire();
+    }
+
+    void Move()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+            return;
+
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
+
+        var rotation = Quaternion.LookRotation(new Vector3(horizontalInput,0,verticalInput));
+        transform.rotation = rotation;
+
+        Vector3 movementDir = transform.forward * Time.deltaTime * movementSpeed;
+        rigidbody.MovePosition(rigidbody.position + movementDir);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            BulletController bullet = collision.gameObject.GetComponent<BulletController>();
+            TakeDamage(bullet.damage);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+        healthBar.value = health;
+        if (health <= 0)
+            PlayerDied();
+
+    }
+
+    void PlayerDied()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void Fire() 
+    {
+        if (Time.time > nextFire) 
+        {
+            nextFire = Time.time + fireRate;
+
+            GameObject bullet = Instantiate(bulletPrefab, bulletPosition.position, Quaternion.identity);
+
+            bullet.GetComponent<BulletController>()?.InitializeBullet(transform.rotation * Vector3.forward);
+
+            AudioManager.Instance.Play3D(playerShootingAudio, transform.position);
+
+            VFXManager.Instance.PlayVFX(bulletFiringEffect, bulletPosition.position);
+
+        }
+    }
+
+}
